1d5373c29acf500e106a547383ce7687512a56e2
KAZIM585 <mohdkazim585@gmail.com>
2024-04-30 23:19:11 +0530

    counter project

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,30 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [counter , setCounter] = useState(0)
+  // let counter = 5
+
+  const addValue = () => {
+    if(counter < 100){
+   counter = counter + 1
+   setCounter(counter)
+    }
+  }
+
+  const decreseValue = () => {
+    if(counter > 0){
+   setCounter(counter - 1)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Counter Project</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value </button>
+      <button onClick={decreseValue}>Decrease value</button>
+      <p>Value start from 0 add value up to 100 only</p>
     </>
   )
 }
